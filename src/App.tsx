5e8b49ced9e3a99427878a6f0cba4d9ffa1b2983
boosterlong <chartrandblake93@gmail.com>
2022-01-01 17:28:08 -0800

    CRT

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,31 @@ import Enemy from './components/enemy';
 import Healthbar from './components/healthbar';
 import Log from './components/log';
 import Spellcard from './components/spellcard';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+</style> 
+
 
 function App() {
   return (
-    <div className="main-container">
-      <div className="enemy-field">
-        <Enemy />
-        <Enemy />
-        <Enemy />
-        <Enemy />
-      </div>
-      <div className="log-field">
-        <Log />
-      </div>
-      <div className="player-field">
-        <Healthbar />
-        <Dicebox />
-        <Spellcard />
-        <Spellcard />
-        <Spellcard />
-        <Spellcard />
-      </div>
+    <div className="main-container crt">
+        <div className="enemy-field">
+            <Enemy />
+            <Enemy />
+            <Enemy />
+            <Enemy />
+        </div>
+        <div className="log-field">
+            <Log />
+        </div>
+        <div className="player-field">
+            <Healthbar />
+            <Dicebox />
+            <Spellcard />
+            <Spellcard />
+            <Spellcard />
+            <Spellcard />
+         </div>
     </div>
   );
 }
