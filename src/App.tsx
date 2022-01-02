085012e8d1e9ab2d72264136581d7128f0888904
Vindexus <vindexus@gmail.com>
2022-01-01 17:31:45 -0800

    Fix merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,18 @@ import Dicebox from './components/dicebox';
 import Enemy from './components/enemy';
 import Healthbar from './components/healthbar';
 import Log from './components/log';
-<<<<<<< HEAD
-import Spellcard from './components/spellcard';
-<<<<<<< HEAD
-<style>
-@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
-</style> 
-
-=======
-import GameContext from "./context/gameContext";
->>>>>>> Added a basic game context
-=======
 import GameContext from "./context/gameContext";
 import SpellList from "./components/spelllist";
->>>>>>> Game context and spell list
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+</style>
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <div className="main-container crt">
-        <div className="enemy-field">
-            <Enemy />
-            <Enemy />
-            <Enemy />
-            <Enemy />
-        </div>
-        <div className="log-field">
-            <Log />
-        </div>
-        <div className="player-field">
-            <Healthbar />
-            <Dicebox />
-            <Spellcard />
-            <Spellcard />
-            <Spellcard />
-            <Spellcard />
-         </div>
-=======
   <GameContext>
-    <div className="main-container">
+    <div className="main-container crt">
       <div className="enemy-field">
         <Enemy />
         <Enemy />
@@ -58,7 +30,6 @@ function App() {
         <Dicebox />
         <SpellList />
       </div>
->>>>>>> Added a basic game context
     </div>
   </GameContext>
   );
